2f44bea5e060f4ae2e4b703d06b1b8e23fa982c2
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-11 16:39:02 +0430

    adding updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 const Navbar = () => {
-  return <Wrapper>Navbar</Wrapper>;
+  return (
+    <Wrapper>
+      <nav>
+        <h1>Sayed Irfan</h1>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Testimonails</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
-  background-color: red;
+  /* background-color: red; */
 `;
 export default Navbar;

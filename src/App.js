1a0700bba80514c92ba1c0e6f7f027cab7d7a33a
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-11 15:52:07 +0430

    adding updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from "react";
+import {
+  Navbar,
+  Sidebar,
+  Hero,
+  About,
+  Services,
+  Projects,
+  Testimonails,
+  Contact,
+} from "./components";
+
+
+
 function App() {
   return (
-    <h1>hello</h1>
-  )
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <About />
+      <Services />
+      <Projects />
+      <Testimonails />
+      <Contact />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Hero from "./Hero";
+import About from "./About";
+import Services from "./Services";
+import Projects from "./Projects";
+import Testimonails from "./Testimonails";
+import Contact from "./Contact";
+
+export {
+  Navbar,
+  Sidebar,
+  Hero,
+  About,
+  Services,
+  Projects,
+  Testimonails,
+  Contact,
+};

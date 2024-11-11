a9a6003c4a63a1870122b27841717c25a5f1a001
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-11 20:10:07 +0430

    adding updates

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,31 +1,55 @@
 import React from "react";
+import styled from "styled-components";
 
 const Sidebar = () => {
+  const isOpen = true;
   return (
-    <div>
-      <h1>Sayed Irfan</h1>
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li>
-          <a href="#">Testimonails</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
+    <Wrapper>
+      <aside className={`${isOpen ? "sidebar" : "show-sidebar "}`}>
+        <h1>Sayed Irfan</h1>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Testimonails</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </aside>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  .sidebar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 50%;
+    height: 100%;
+    background-color: gray;
+    transition: all 0.3s linear;
+    transform: translate(-100);
+    box-shadow: 0.25em 0.25em 0.75em rgba(0, 0, 0, 0.25),
+      0.125em 0.125em 0.25em rgba(0, 0, 0, 0.15);
+  }
+
+  .show-sidebar {
+    transform: translate(0);
+    z-index: 999;
+  }
+`;
 
 export default Sidebar;

34d8c488a88e7fef92a198c50e36bc692a18f957
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-28 18:58:27 +0430

    adding updates

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,18 @@ import styled from "styled-components";
 const About = () => {
   return (
     <Wrapper>
-      <div></div>
+      <div>
+        <p>About me</p>
+
+        <h1>About Me</h1>
+        <h3>
+          Experienced graphic designer with +5 years of experience proficient in
+          Adobe Creative Suite, brand identity development, typography, print
+          and digital design, illustration, UI/UX design, photo editing, Motion
+          design, visual communication, attention to detail, collaboration, and
+          problem-solving. Let's create impactful visuals together
+        </h3>
+      </div>
       <div></div>
     </Wrapper>
   );

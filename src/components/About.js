8ead9eab1ad4f96592b7cb439fd3aca2134f12f0
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-28 19:31:56 +0430

    adding updates

--- src/components/About.js
+++ src/components/About.js
@@ -4,27 +4,36 @@ import styled from "styled-components";
 const About = () => {
   return (
     <Wrapper>
-      <div>
-        <p>About me</p>
+      <div className="container">
+        <div>
+          <p className="about">About me</p>
 
-        <h1>About Me</h1>
-        <h3>
-          Experienced graphic designer with +5 years of experience proficient in
-          Adobe Creative Suite, brand identity development, typography, print
-          and digital design, illustration, UI/UX design, photo editing, Motion
-          design, visual communication, attention to detail, collaboration, and
-          problem-solving. Let's create impactful visuals together
-        </h3>
-      </div>
-      <div>
-        <label>Graphic Design</label>
+          <h1>About Me</h1>
+          <h3>
+            Experienced graphic designer with +5 years of experience proficient
+            in Adobe Creative Suite, brand identity development, typography,
+            print and digital design, illustration, UI/UX design, photo editing,
+            Motion design, visual communication, attention to detail,
+            collaboration, and problem-solving. Let's create impactful
+            visuals together
+          </h3>
+        </div>
+        <div>
+          <label>Graphic Design</label>
+        </div>
       </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-  background-color: red;
+
+.container{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
 `;
 
 export default About;

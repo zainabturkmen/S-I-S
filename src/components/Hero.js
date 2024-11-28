10e3add695d39cb3535f95b51872978e9018393a
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-28 18:27:23 +0430

    adding updates

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -256,6 +256,9 @@ const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
   }
 
   .content {

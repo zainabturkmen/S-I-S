2c5464200f16d37f3c488e9c36216d059658695a
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-30 10:11:55 +0430

    adding updates

--- src/components/Testimonails.js
+++ src/components/Testimonails.js
@@ -1,12 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import Marquee from "react-fast-marquee";
 
 const Testimonails = () => {
-  return <Wrapper>Testimonails</Wrapper>;
+  return (
+    <Wrapper>
+      <div className="container">
+        <h1>Hear About Us</h1>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
-  background-color: red;
+
+  h1 {
+    text-align: center;
+    margin-top: 2em;
+    font-family: Inter;
+    font-size: 30px;
+    font-weight: 600;
+    line-height: 78px; /* 260% */
+    margin-bottom: 0;
+  }
+
+  @media (min-width: 1000px) {
+    h1 {
+      font-size: 40px;
+      font-weight: 600;
+    }
+  }
 `;
 
 export default Testimonails;

8d8d85b48f6fbc799647557ca52a589451fc0bc4
zainabturkmen <zainabturkmen7@gmail.com>
2024-12-05 10:05:09 +0430

    adding updates

--- src/components/Testimonails.js
+++ src/components/Testimonails.js
@@ -1,19 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 import Marquee from "react-fast-marquee";
+import star from "../assets/Shape.svg";
+import user from "../assets/Ellipse 74.svg";
+import user1 from "../assets/Ellipse 74 (1).svg";
+import user2 from "../assets/Ellipse 74 (2).svg";
 
 const Testimonails = () => {
   return (
     <Wrapper>
+      <h1>Hear from our customers</h1>
       <div className="container">
-        <h1>Hear from our customers</h1>
+        <div className="customer">
+          <img src={star} />
+          <p className="reveiew">
+            We will also facilitate the business marketing of these products
+            with our SEO experts so that they become a ready to use website &
+            help sell product from company
+          </p>
+          <div className="user">
+            <img src={user} alt="user" />
+            <div className="user-info">
+              <h3>Amir Uddin</h3>
+              <p>UX Designer </p>
+            </div>
+          </div>
+        </div>
       </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
-
   h1 {
     text-align: center;
     margin-top: 2em;
@@ -22,6 +40,18 @@ const Wrapper = styled.div`
     font-weight: 600;
     line-height: 78px; /* 260% */
     margin-bottom: 0;
+  }
+
+  .customer {
+  }
+
+  .reveiew {
+    color: var(--Light-Text-Color-Title, #11142d);
+    font-family: Inter;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 120%; /* 16.8px */
+    width: 418.211px;
   }
 
   @media (min-width: 1000px) {
